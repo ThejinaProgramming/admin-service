61795c929523e46d2aff6a9eaeddace678ad6c40
Thejina <thejinaprogramming@gmail.com>
2023-12-30 05:47:39 +0530

    Comments were added

--- router/admin_router.py
+++ router/admin_router.py
@@ -7,6 +7,9 @@ admin_router = APIRouter()
 
 @admin_router.get("/reservation/ticket/{flight_id}")
 async def get_reservation_by_flight_id(flight_id: str):
+    """
+    Search all the reservations for a given flight id
+    """
     return await AdminService().view_reservation_by_flight(flight_id)
 
 
@@ -14,9 +17,15 @@ async def get_reservation_by_flight_id(flight_id: str):
 async def get_reservation_by_passenger(
         passenger_name: str = Query(..., description="passenger name"),
         passenger_email: str = Query(..., description="passenger email address")):
+    """
+    Search all the reservations for a given user details
+    """
     return await AdminService().view_reservation_by_user(passenger_name, passenger_email)
 
 
 @admin_router.put("/reservation/ticket/{reservation_id}")
 async def update_reservation(reservation_id: str, ticket_model: TicketModificationModel):
-        return await AdminService().update_reservation(reservation_id, ticket_model)
+    """
+    Modify a reservation - user name and email address can only be modified
+    """
+    return await AdminService().update_reservation(reservation_id, ticket_model)
--- service/admin_service.py
+++ service/admin_service.py
@@ -12,22 +12,29 @@ class AdminService:
     async def view_reservation_by_flight(self, flight_id: str):
         # Search for reservation based on the flight id
         reservation = await collection.find({"flight_id": flight_id}).to_list(length=None)
-        for obj in reservation:
-            obj.pop('_id', None)
+        if len(reservation) == 0:
+            raise HTTPException(status_code=404, detail="Reservation not found")
+        else:
+            for obj in reservation:
+                obj.pop('_id', None)
 
-        return reservation
+            return reservation
 
     async def view_reservation_by_user(self, passenger_name: str, passenger_email: str):
         # search for reservation based on the user
         search_query = {"passenger_name": passenger_name, "passenger_email": passenger_email}
         reservation = await collection.find(search_query).to_list(length=None)
-        for obj in reservation:
-            obj.pop('_id', None)
+        if len(reservation) == 0:
+            raise HTTPException(status_code=404, detail="Reservation not found")
+        else:
+            for obj in reservation:
+                obj.pop('_id', None)
 
-        return reservation
+            return reservation
 
     async def update_reservation(self,
                                  reservation_id: str, ticket_model: TicketModificationModel):
+        # Update reservation details
         existing_reservation = await collection.find_one({"_id": ObjectId(reservation_id)})
         if existing_reservation is None:
             raise HTTPException(status_code=404, detail="Reservation not found")
@@ -36,5 +43,3 @@ class AdminService:
                                                                        {"$set": ticket_model.dict()})
             updated_reservation.pop('_id', None)
             return {"message": "Reservation updated successfully", "updated_reservation": updated_reservation}
-
-
